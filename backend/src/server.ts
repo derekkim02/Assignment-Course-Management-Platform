66c1f0ca08f7a4de35ea5e118cb1e57d9965414e
z5362831 <z5362831@ad.unsw.edu.au>
2024-10-04 14:35:36 +1000

    renamed index.ts to server.ts, added backend entry points in server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+/// AUTHENTICATION
+app.post('/api/auth/login', (req, res) => {
+  res.json({ token: '1234' });
+});
+
+app.post('/api/auth/logout', (req, res) => {
+  res.json({ message: 'Logged out' });
+});
+
+app.post('/api/auth/register', (req, res) => {
+  res.json({ message: 'Registered' });
+});
+
+/// HOMEPAGES
+app.get('/api/student/homepage', (req, res) => {
+  res.json({ user: { name: 'John Doe' } });
+});
+
+app.get('/api/tutor/homepage', (req, res) => {
+  res.json({ user: { name: 'Jane Doe' } });
+});
+
+app.get('/api/lecturer/homepage', (req, res) => {
+  res.json({ user: { name: 'Andrew Taylor' } });
+});
+
+/// ASSIGNMENT MANAGEMENT
+app.post('/api/lecturer/create-assignment', (req, res) => {
+  res.json({ message: 'Course created' });
+});
+
+app.put('/api/lecturer/update-assignment', (req, res) => {
+  res.json({ message: 'Course updated' });
+});
+
+app.delete('/api/lecturer/delete-assignment', (req, res) => {
+  res.json({ message: 'Course deleted' });
+});
+
+app.get('/api/view-assignments', (req, res) => {
+  res.json({ assignments: [{ title: 'Assignment 1' }] });
+});
+
+
+/// STUDENT ASSIGNMENT MANAGEMENT
+app.post('/api/student/submit-assignment', (req, res) => {
+  res.json({ message: 'Assignment submitted' });
+});
+
+app.get('/api/student/view-marks', (req, res) => {
+  res.json({ assignment: { title: 'Assignment 1' } });
+});
+
+/// TUTOR ASSIGNMENT MANAGEMENT
+
+app.put('/api/tutor/edit-mark', (req, res) => {
+  res.json({ assignments: [{ title: 'Assignment 1' }] });
+});
+
+app.get('/api/tutor/view-assignments', (req, res) => {
+  res.json({ assignments: [{ title: 'Assignment 1' }] });
+});
+
+app.get('/api/tutor/student-search', (req, res) => {
+  res.json({ assignment: { title: 'Assignment 1' } });
+});
