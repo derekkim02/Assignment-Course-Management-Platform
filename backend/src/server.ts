69b3259e0374585f0113c4374cb2385998af7445
Alex Kim <alexkim@Alexs-MacBook-Air.local>
2024-10-22 20:33:48 +1100

    WIP

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request } from 'express';
 import cors from 'cors';
-import { Prisma, PrismaClient, User } from '@prisma/client';
+import { Prisma, PrismaClient, Trimester, User } from '@prisma/client';
 import { createAssessment } from './assessments';
 
 import jwt from 'jsonwebtoken';
@@ -125,7 +125,6 @@ app.post('/api/auth/register', async (req, res) => {
 /// HOMEPAGES
 app.get('/api/courses', verifyToken, async (req, res) => {
   try {
-    // TODO: Find courses based on which role and stuff.
     const courses = await prisma.course.findMany();
     res.json(courses);
   } catch (e) {
@@ -137,8 +136,7 @@ app.get('/api/enrollments', verifyToken, async (req, res) => {
   try {
     const user = await getUserFromToken(req);
     const role = req.query.role as string;
-    const fetchCourses = courseFetchStrategies[role];
-    const courses = await fetchCourses(user);
+    const courses = await courseFetchStrategies[role](user);
 
     res.json(courses);
   } catch (e) {
@@ -149,26 +147,29 @@ app.get('/api/enrollments', verifyToken, async (req, res) => {
 
 app.get('/api/users', verifyToken, checkIgiveAdmin, async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (e) {
-    res.status(500).json({ error: 'Failed to fetch courses' });
-  }
-});
-
-app.post('/api/courses', verifyToken, checkIgiveAdmin, async (req, res) => {
-  const { name, code, description } = req.body;
-  try {
-    const newCourse = await prisma.course.create({
-      data: {
-        name,
-        code,
-        description
+    const users = await prisma.user.findMany({
+      select: {
+        zid: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        admin: {
+          select: {
+            zid: true
+          }
+        }
       }
     });
-    res.json(newCourse);
+    const transformedUsers = users.map(user => ({
+      zid: user.zid,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      isAdmin: user.admin.length !== 0
+    }));
+    res.json(transformedUsers);
   } catch (e) {
-    res.status(400).json({ error: 'Failed to create course' });
+    res.status(500).json({ error: 'Failed to fetch courses' });
   }
 });
 
@@ -238,4 +239,116 @@ app.get('/api/tutor/student-search', (req, res) => {
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
   process.exit();
+});
+
+// ADMIN SETTINGS
+app.put('/api/admin/change-role/:userId', verifyToken, checkIgiveAdmin, async (req, res) => {
+  const { userId } = req.params;
+  const { isAdmin } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { zid: parseInt(userId) }
+    });
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
+    if (isAdmin) {
+      await prisma.admin.create({
+        data: {
+          zid: user.zid
+        }
+      });
+    } else {
+      const adminCount = await prisma.admin.count();
+      if (adminCount === 1) {
+        res.status(400).json({ error: 'Cannot demote the last admin user' });
+        return;
+      }
+      await prisma.admin.delete({
+        where: {
+          zid: user.zid
+        }
+      });
+    }
+
+    res.json({ message: 'User role updated successfully' });
+  } catch (error) {
+    console.error('Error updating user role:', error);
+    res.status(500).json({ error: 'Failed to update user role' });
+  }
+});
+
+app.post('/api/admin/course-offerings', verifyToken, checkIgiveAdmin, async (req, res) => {
+  function mapTermToTrimester(term: number): Trimester {
+    switch (term) {
+      case 0:
+        return Trimester.T0;
+      case 1:
+        return Trimester.T1;
+      case 2:
+        return Trimester.T2;
+      case 3:
+        return Trimester.T3;
+      default:
+        return Trimester.T0;
+    }
+  }
+
+
+  const { lecturerId, courseId, termYear, termTerm } = req.body;
+  try {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId }
+    });
+
+    if (!course) {
+      res.status(404).json({ error: 'Course not found' });
+      return;
+    }
+
+    await prisma.term.upsert({
+      where: {
+        year_term: {
+          year: termYear,
+          term: mapTermToTrimester(termTerm)
+        }
+      },
+      update: {},
+      create: {
+        year: termYear,
+        term: mapTermToTrimester(termTerm)
+      }
+    });
+
+    await prisma.teachingAssignment.create({
+      data: {
+        lecturerId,
+        courseId,
+        termYear,
+        termTerm: mapTermToTrimester(termTerm)
+      }
+    });
+    res.json({ message: 'Course offering created successfully' });
+  } catch (error) {
+    console.error('Error creating course offering:', error);
+    res.status(500).json({ error: 'Failed to create course offering' });
+  }
+});
+
+app.post('/api/admin/courses', verifyToken, checkIgiveAdmin, async (req, res) => {
+  const { name, code, description } = req.body;
+  try {
+    const newCourse = await prisma.course.create({
+      data: {
+        name,
+        code,
+        description
+      }
+    });
+    res.json(newCourse);
+  } catch (e) {
+    res.status(400).json({ error: 'Failed to create course' });
+  }
 });
