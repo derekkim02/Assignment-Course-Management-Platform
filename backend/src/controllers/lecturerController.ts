883c671dc5e8e64bbc06366692424117648217db
Justin <justinrockz11@gmail.com>
2024-10-29 07:58:32 +1100

    completed some lecture controller functions

--- backend/src/controllers/lecturerController.ts
+++ backend/src/controllers/lecturerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createAssessment } from "../assessments";
+import prisma from '../prismaClient';
 
 export const createAssignment =  async (req: Request, res: Response): Promise<void> => {
 	const {lecturerId, title, description, dueDate, term, courseID, } = req.body;
@@ -9,4 +10,58 @@ export const createAssignment =  async (req: Request, res: Response): Promise<vo
 	} catch (error) {
 	  res.status(400).json({ error: (error as Error).message });
 	}
-  }
+}
+
+export const viewSubmission =  async (req: Request, res: Response): Promise<void> => {
+	const { submissionId } = req.body;
+
+	try {
+	  const submission = await prisma.submission.findUnique({where: {id: parseInt(submissionId)}});
+
+    if (!submission) {
+      res.status(404).json({ error: 'Submission not found' });
+      return;
+    }
+	  res.json(submission);
+	} catch (error) {
+	  res.status(400).json({ error: (error as Error).message });
+	}
+}
+
+export const getStudents =  async (res: Response): Promise<void> => {
+	try {
+	  const students = await prisma.user.findMany({
+		where: {
+		  isAdmin: false,
+		},
+	  });
+
+    if (!students) {
+      res.status(404).json({ error: 'Students table not found' });
+      return;
+    }
+	  res.json(students);
+	} catch (error) {
+	  res.status(400).json({ error: (error as Error).message });
+	}
+}
+
+export const searchStudentById =  async (req: Request, res: Response): Promise<void> => {
+	const { studentId } = req.body;
+
+	try {
+		const student = await prisma.user.findMany({
+		  where: {
+			zid: studentId,
+		  },
+		});
+
+	  if (!student) {
+		res.status(404).json({ error: 'Student not found' });
+		return;
+	  }
+		res.json(student);
+  } catch (error) {
+    res.status(400).json({ error: (error as Error).message });
+	}
+}
