efb93dfcbbcf9c646e736bcce0cf637a69f5a0a9
Derek Kim <z5362831@ad.unsw.edu.au>
2024-10-29 23:39:47 +1100

    added new types

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,6 +1,18 @@
 declare namespace Express {
 	export interface Request {
+		// User email in every request
 		userEmail?: string;
-		zid?: number;
+		submissionInfo?: SubmissionInfo;
 	}
+}
+
+interface TestCase {
+	input: string;
+	expectedOutput: string;
+}
+
+interface SubmissionInfo {
+	zid: number;
+	shCmd: string;
+	testCases: TestCase[];
 }
