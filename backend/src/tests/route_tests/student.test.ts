e285106aa863d27125068e06f06eccedbdca7905
Derek Kim <z5362831@ad.unsw.edu.au>
2024-11-01 07:44:41 +1100

    sample student submissiontest made

--- backend/src/tests/route_tests/student.test.ts
+++ backend/src/tests/route_tests/student.test.ts
@@ -8,19 +8,19 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-beforeAll(() => {
-	resetDatabase();
+beforeAll(async () => {
+	await resetDatabase();
 });
 
-describe('Student Routes', () => {
-	describe('POST /assignments/:assignmentId/submit', () => {
-		const py3FilePath = path.join(__dirname, '..', 'sample_assignments', 'python3SampleAssignment.tar.gz');
+describe('POST api/student/assignments/:assignmentId/submit', () => {
+	const py3FilePath = path.join(__dirname, '..', 'sample_assignments', 'python3SampleAssignment.tar.gz');
 
-		it('should submit an individual assignment', async () => {
-			const response = await request(app)
-				.post('/assignments/123/submit')
-				.attach('python3example', py3FilePath)
-				.expect(200);
-		});
+	it('should fail because the assignment does not exist', async () => {
+		const response = await request(app)
+			.post('/api/student/assignments/123/submit')
+			.attach('python3example', py3FilePath)
+			.expect(401)
+			.expect('Content-Type', /json/)
+			.expect({ message: 'No token provided' });
 	});
 });
