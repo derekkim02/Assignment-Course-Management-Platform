ab318540c5777af29333ce6c83c9de1be4977766
z5362831 <z5362831@ad.unsw.edu.au>
2024-11-01 14:24:19 +1100

    WIP

--- backend/src/tests/route_tests/student.test.ts
+++ backend/src/tests/route_tests/student.test.ts
@@ -19,24 +19,46 @@ const generateDbData = async (): Promise<void> => {
 		password: 'bananas',
 		cpassword: 'bananas'
 	};
+	const user2RegData = {
+		firstName: 'user', 
+		lastName: '2',
+		email: 'user2@gmail.com',
+		password: 'bananas',
+		cpassword: 'bananas'
+	};
 
 	// Register a user, should be admin since db is fresh
 	await request(app)
 		.post('/api/auth/register')
 		.send(user1RegData)
 		.expect(201);
+	// Register student user
+	await request(app)
+		.post('/api/auth/register')
+		.send(user2RegData)
+		.expect(201);
 
 	// Get the token by logging in
-	const loginData = {
+	const login1 = {
 		email: user1RegData.email,
 		password: user1RegData.password
 	}
-	const loginResponse = await request(app)
+	const login2 = {
+		email: user2RegData.email,
+		password: user2RegData.password
+	}
+	const user1 = await request(app)
 		.post('/api/auth/login')
-		.send(loginData)
+		.send(login1)
 		.expect(200);
-	const token = loginResponse.body.token;
-	
+	const user2 = await request(app)
+		.post('/api/auth/login')
+		.send(login2)
+		.expect(200);
+
+	const token1 = user1.body.token;
+	const token2 = user2.body.token;
+
 	// Create a course
 	const courseData = {
 		name: 'Programming Fundamentals',
@@ -44,11 +66,36 @@ const generateDbData = async (): Promise<void> => {
 		description: 'This course is an introductory course to the basics of Computer Programming and Computer Science. ' +
 		             'It is intended as an introduction to studying further in Computer Science or related fields.'
 	}
-	await request(app)
+
+	// Admin creates a course
+	const course = await request(app)
 		.post('/api/admin/courses')
-		.set('authorization', `Bearer ${token}`)
+		.set('authorization', `Bearer ${token1}`)
 		.send(courseData)
 		.expect(201);
+	
+	// Admin creates a course offering
+	const courseOfferingData = {
+		courseId: course.body.id,
+		termYear: 2024,
+		termTerm: 1,
+		lecturerId: user1.body.id
+	}
+	await request(app)
+		.post('/api/admin/course-offerings')
+		.set('authorization', `Bearer ${token1}`)
+		.send(courseOfferingData)
+		.expect(201);
+	
+	// Admin creates an assignment
+	const assignmentData = {
+		courseId: course.body.id,
+		title: 'Python 3 Example',
+		description: 'This is an example assignment for Python 3',
+		maxMark: 10,
+		dueDate: new Date(),
+	}
+	
 }
 
 describe('POST api/student/assignments/:assignmentId/submit', () => {
