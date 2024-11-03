dc44e10687378e23a737f71bb75def5a9cb8e11c
Alex Kim <alexkim5682@gmail.com>
2024-11-03 21:38:43 +1100

    Added documentation

--- backend/src/routes/adminRouter.ts
+++ backend/src/routes/adminRouter.ts
@@ -11,8 +11,50 @@ router.post('/course-offerings', createEnrollment);
 router.get('/courses', getCourses);
 router.post('/courses', createCourse);
 router.get('/users', getUsers);
+
+/**
+ * @route GET /course-offerings
+ * @description Fetch all course offerings.
+ * @header {string} Authorization Bearer token for authentication. Format: `Bearer {token}`.
+ * @returns {object[]} 200 - List of course offerings
+ * @returns {string} 200.id - Unique identifier of the course offering
+ * @returns {string} 200.courseCode - Course code
+ * @returns {string} 200.courseName - Course name
+ * @returns {string} 200.term - Term of the course offering in the format `yearTerm`
+ * @returns {object} 500 - Internal server error
+ * @returns {string} 500.error - Error message
+ */
 router.get('/course-offerings', getCourseOfferings);
+
+/**
+ * @route GET /course-offerings/:courseOfferingId
+ * @description Fetch a specific course offering.
+ * @header {string} Authorization Bearer token for authentication. Format: `Bearer {token}`.
+ * @param {string} courseOfferingId.path.required - The unique identifier of the course offering
+ * @returns {object} 200 - Course offering
+ * @returns {string} 200.id - Unique identifier of the course offering
+ * @returns {string} 200.courseCode - Course code
+ * @returns {string} 200.courseName - Course name
+ * @returns {string} 200.term - Term of the course offering in the format `yearTerm`
+ * @returns {User} 200.lecturer - Lecturer for the course offering.
+ * @returns {User[]} 200.students - list of students enrolled in the course offering.
+ * @returns {User[]} 200.tutors - list of tutors for the course offering.
+ * @returns {object} 500 - Internal server error
+ * @returns {string} 500.error - Error message
+ */
 router.get('/course-offerings/:courseOfferingId', getCourseOffering);
+
+/**
+ * @route PUT /course-offerings/:courseOfferingId
+ * @description Update a specific course offering.
+ * @header {string} Authorization Bearer token for authentication. Format: `Bearer {token}`.
+ * @param {string} courseOfferingId.path.required - The unique identifier of the course offering
+ * @param {string} lecturerId.body.required - The unique identifier of the lecturer
+ * @param {string[]} studentIds.required - The unique identifiers of the students
+ * @param {string[]} tutorIds.required - The unique identifiers of the tutors
+ * @param {string} courseId.body.required - The unique identifier of the course
+ * @returns 200 - Course offering
+ */
 router.put('/course-offerings/:courseOfferingId', updateCourseOffering);
 
 export default router;
