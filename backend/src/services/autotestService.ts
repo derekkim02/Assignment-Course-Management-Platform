fc53e4fbb4027d8fb0116b0c353d2bb662038146
Derek Kim <z5362831@ad.unsw.edu.au>
2024-11-05 22:08:42 +1100

    wip

--- backend/src/services/autotestService.ts
+++ backend/src/services/autotestService.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 class AutotestService {
     public static testEnvDir = path.join(__dirname, "..", "..", "app", "test_env");
 
