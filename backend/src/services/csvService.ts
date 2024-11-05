dfe15b3e1e24361497031215511813234c2e5778
Derek Kim <z5362831@ad.unsw.edu.au>
2024-11-05 21:04:23 +1100

    completed csv import service

--- backend/src/services/csvService.ts
+++ backend/src/services/csvService.ts
@@ -1,15 +1,15 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 import prisma from '../prismaClient';
-import { Day } from "@prisma/client"
+import { Day, User, Class } from '@prisma/client';
 interface SMSCsvRow {
-	fullName: string;
-	zid: string;
+	fullname: string;
+	zId: string;
 	email: string;
-	classId: number;
+	classId: string;
 	className: string;
 	startTime: string;
-	duration: number;
+	duration: string;
 	day: string;
 	tutorId: string;
 	tutorName: string;
@@ -17,6 +17,9 @@ interface SMSCsvRow {
 }
 
 const splitFullName = (fullName: string): { firstName: string; lastName: string } => {
+	if (!fullName) {
+		throw new Error(fullName);
+	}
 	const names = fullName.split(' ');
 	return {
 		firstName: names[0],
@@ -37,77 +40,97 @@ const dayToEnum = (day: string): Day => {
 
 class CsvService {
 	public static async importSMSCsvToDbForCourseOffering(csvFilePath: string, courseOfferingId: number): Promise<void> {
-		fs.createReadStream(csvFilePath).pipe(csv()).on('data', async (data: SMSCsvRow) => {
-			const { firstName, lastName } = splitFullName(data.fullName);
+		const students: User[] = [];
+		const tutors: User[] = [];
+		const classes: Class[] = [];
+		const courseOfferingUpdates: {
+			courseOfferingId: number,
+			classId: number,
+			studentId: number,
+			tutorId: number
+		}[] = [];
+
+		fs.createReadStream(csvFilePath)
+		.pipe(csv())
+		.on('data', async (data: SMSCsvRow) => {
+			const { firstName, lastName } = splitFullName(data.fullname);
 			const { firstName: tutorFirstName, lastName: tutorLastName } = splitFullName(data.tutorName);
 			const defaultPassword = 'default_password';
-			const studentId = parseInt(data.zid.replace('z', ''), 10);
+			const studentId = parseInt(data.zId.replace('z', ''), 10);
 			const tutorId = parseInt(data.tutorId.replace('z', ''), 10);
+			const classId = parseInt(data.classId, 10);
+			const duration = parseInt(data.duration, 10);
 
+			students.push({
+				zid: studentId,
+				firstName,
+				lastName,
+				email: data.email,
+				password: defaultPassword,
+				isAdmin: false,
+			});
+
+			tutors.push({
+				zid: tutorId,
+				firstName: tutorFirstName,
+				lastName: tutorLastName,
+				email: data.tutorEmail,
+				password: defaultPassword,
+				isAdmin: false,
+			});
+
+			classes.push({
+				id: classId,
+				name: data.className,
+				startTime: data.startTime,
+				duration: duration,
+				day: dayToEnum(data.day),
+				tutorId: tutorId,
+				courseOfferingId,
+			});
+
+			courseOfferingUpdates.push({
+				courseOfferingId,
+				classId,
+				studentId,
+				tutorId,
+			});
+		}).on('end', async () => {
 			try {
-				const student = await prisma.user.upsert({
-					where: { zid: studentId },
-					update: {},
-					create: {
-						zid: studentId,
-						firstName,
-						lastName,
-						email: data.email,
-						password: defaultPassword,
-					},
-				});
+				await prisma.$transaction([
+					prisma.user.createMany({ data: students, skipDuplicates: true }),
+					prisma.user.createMany({ data: tutors, skipDuplicates: true }),
+					prisma.class.createMany({ data: classes, skipDuplicates: true }),
+				]);
 
-				const tutor = await prisma.user.upsert({
-					where: { zid: tutorId },
-					update: {},
-					create: {
-						zid: tutorId,
-						firstName: tutorFirstName,
-						lastName: tutorLastName,
-						email: data.tutorEmail,
-						password: defaultPassword,
-					},
-				});
-
-				const classOffering = await prisma.class.upsert({
-					where: { id: data.classId },
-					update: {
-						students: {
-							connect: { zid: student.zid },
+				for (const { courseOfferingId, classId, studentId, tutorId } of courseOfferingUpdates) {
+					await prisma.class.update({
+						where: { id: classId },
+						data: {
+							students: {
+								connect: { zid: studentId },
+							},
 						},
-					},
-					create: {
-						id: data.classId,
-						name: data.className,
-						startTime: data.startTime,
-						duration: data.duration,
-						day: dayToEnum(data.day),
-						tutorId: tutor.zid,
-						courseOfferingId,
-						students: {
-							connect: { zid: student.zid },
+					});
+					await prisma.courseOffering.update({
+						where: { id: courseOfferingId },
+						data: {
+							classes: {
+								connect: { id: classId },
+							},
+							enrolledStudents: {
+								connect: { zid: studentId },
+							},
+							tutors: {
+								connect: { zid: tutorId },
+							},
 						},
-					},
-				});
-
-				await prisma.courseOffering.update({
-					where: { id: courseOfferingId },
-					data: {
-						classes: {
-							connect: { id: classOffering.id },
-						},
-						enrolledStudents: {
-							connect: { zid: student.zid },
-						},
-						tutors: {
-							connect: { zid: tutor.zid },
-						},
-					},
-				});
-
-			} catch (error) {
-				console.error(error);
+					});
+				}
+			} catch (err) {
+				console.error(err);
 			}
+			
 		});
 	}
 }
