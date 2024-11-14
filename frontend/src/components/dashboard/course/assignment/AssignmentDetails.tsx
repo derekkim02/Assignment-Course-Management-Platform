17e06bcc36c349a54db494b1d2d0097357ff3d59
Justin <justinrockz11@gmail.com>
2024-11-14 22:31:37 +1100

    feat: implemented download assessment grades

--- frontend/src/components/dashboard/course/assignment/AssignmentDetails.tsx
+++ frontend/src/components/dashboard/course/assignment/AssignmentDetails.tsx
@@ -116,7 +116,6 @@ const AssignmentDetails: React.FC = () => {
         throw new Error('Failed to mark submissions');
       }
       refetchAssignment();
-      console.log(response);
       message.success('Submissions marked successfully!');
     })
       .catch((error) => {
@@ -133,7 +132,6 @@ const AssignmentDetails: React.FC = () => {
     setViewSubmissions(true);
   };
 
-  // Handle search
   const handleSearch = (query: string) => {
     setSearchQuery(query);
     const filtered: User[] = courseOffering.enrolledStudents.filter((student: User) =>
@@ -144,6 +142,38 @@ const AssignmentDetails: React.FC = () => {
     setFilteredStudents(filtered);
     setCurrentPage(1);
   };
+
+  const handleCollectGrades = () => {
+    message.info('Downloading grades...');
+    try {
+      fetch(`${config.backendUrl}/api/lecturer/assignments/${assignmentId}/grades`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (response) => {
+        if (!response.ok) {
+          throw new Error('Failed to collect grades!');
+        }
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `assignment${assignmentId}_grades.tar.gz`; // Set the desired file name
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+        message.success('Grades collected successfully!');
+      })
+        .catch((error) => {
+          message.error(`Failed to download grades: ${error}`);
+        });
+    } catch (error) {
+      console.error('Error downloading file:', error);
+      message.error('Failed to download file.');
+    }
+  }
 
   // Handle pagination
   const handlePageChange = (page: number, size?: number) => {
@@ -213,8 +243,12 @@ const AssignmentDetails: React.FC = () => {
                 Create Autotest
               </Button>
 
-              <Button type="primary" onClick={handleMarkSubmissions} style={{ marginBottom: '20px', width: '120px', alignSelf: 'center' }}>
+              <Button type="primary" onClick={handleMarkSubmissions} style={{ marginBottom: '20px', width: '120px', alignSelf: 'center', marginRight: '10px' }}>
                 Mark Submissions
+              </Button>
+
+              <Button type="primary" onClick={handleCollectGrades} style={{ marginBottom: '20px', width: '120px', alignSelf: 'center' }}>
+                Collect Grades
               </Button>
             </div>
           </>
